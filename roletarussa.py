8e033b1837c4556c172a4d74412399ae5cb5286a
profile <>
2024-08-20 16:42:48 -0300

    Update roletarussa.py

--- roletarussa.py
+++ roletarussa.py
@@ -2,28 +2,28 @@ jogador1 = input("\nPedra Papel ou Tesoura? ").lower()
 jogador2 = input("\nPedra Papel ou Tesoura? ").lower()
 jogo = True
 continuarJogo = "sim".lower()
-while continuarJogo == "sim":
-    if jogador1 == "pedra":
-        if jogador2 == "pedra":
-            print("Empate")
-        if jogador2 == "papel":
-            print("Jogador 2 ganhou")
-        if jogador2 == "tesoura":
-            print("Jogador 1 ganhou")
-    elif jogador1 == "tesoura":
-        if jogador2 == "pedra":
-            print("Jogador 2 ganha")
-        if jogador2 == "papel":
-            print("Jogador 1 ganhou")
-        if jogador2 == "tesoura":
-            print("Empate")
-    elif jogador1 == "papel":
-        if jogador2 == "pedra":
-            print("Jogador 1 ganha")
-        if jogador2 == "papel":
-            print("Empate")
-        if jogador2 == "tesoura":
-            print("Jogador 2 ganha")
-    continuarJogo = input("Deseja continuar o jogo? Sim/Nao ")
-    if continuarJogo == "sim":
-        jogo = True
+
+if jogador1 == "pedra":
+
+    if jogador2 == "pedra":
+        print("Empate")
+    if jogador2 == "papel":
+        print("Jogador 2 ganhou")
+    if jogador2 == "tesoura":
+        print("Jogador 1 ganhou")
+
+elif jogador1 == "tesoura":
+    if jogador2 == "pedra":
+        print("Jogador 2 ganha")
+    if jogador2 == "papel":
+        print("Jogador 1 ganhou")
+    if jogador2 == "tesoura":
+        print("Empate")
+        
+elif jogador1 == "papel":
+    if jogador2 == "pedra":
+        print("Jogador 1 ganha")
+    if jogador2 == "papel":
+        print("Empate")
+    if jogador2 == "tesoura":
+        print("Jogador 2 ganha")
